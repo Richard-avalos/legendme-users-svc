36b5985bf09fd543739fbeeb55f940f7e6952cbe
Richard Avalos <130488668+Richard-avalos@users.noreply.github.com>
2025-10-11 19:13:10 -0600

    Estructura inicial

---
+++ src/main/java/com/legendme/users/svc/LegendmeUsersSvcApplication.java
@@ -0,0 +1,13 @@
+package com.legendme.users.svc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegendmeUsersSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LegendmeUsersSvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/legendme/users/svc/LegendmeUsersSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.legendme.users.svc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LegendmeUsersSvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
